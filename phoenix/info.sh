ff78ea0fa0b03dcd57f5ac8fc235afd2b2314ddf
sreekfreak995 <sreekfreak995@gmail.com>
2020-08-17 20:09:34 +0530

    phoenix: 2020-08-17 update
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- phoenix/info.sh
+++ phoenix/info.sh
@@ -1,4 +1,4 @@
-DEVICE_NAME="Poco X2/Redmi K30"	
-TGNAME="CryllicBuster273"	
-MAINTAINER="CryllicBuster273"	
+DEVICE_NAME="Poco X2/Redmi K30"
+TGNAME="sreekfreak995"
+MAINTAINER="CryllicBuster273 and Sreekanth"
 DISCUSSION="https://t.me/derpphoenix"
